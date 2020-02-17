d8bfbe224e02e261f3c5c94fec6e77d88614ed6a
IvanPieVodka <59840386+IvanPieVodka@users.noreply.github.com>
2020-02-17 11:27:40 -0500

    Add files via upload

---
+++ informatics/src/fmi/informatics/enums/EType.java
@@ -0,0 +1,8 @@
+package fmi.informatics.enums;
+
+// Промяна на CustomDialog да приема EType аргумент за типа на диалога – филтриране или сортиране.
+public enum EType {
+	SORT, 
+	FILTER,
+	RESET
+}
